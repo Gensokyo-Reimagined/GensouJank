5eb4ca392fb04be1f5f6ee74fec21c9eee4f5274
DoggySazHi <reimu@williamle.com>
2023-12-17 22:37:40 -0800

    what is this, reflection hell?

--- src/main/java/net/gensokyoreimagined/gensoujank/TouhouHitboxes.java
+++ src/main/java/net/gensokyoreimagined/gensoujank/TouhouHitboxes.java
@@ -1,5 +1,7 @@
 package net.gensokyoreimagined.gensoujank;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.minecraft.server.network.CommonListenerCookie;
 import net.minecraft.server.players.PlayerList;
 import org.bukkit.*;
@@ -40,49 +42,72 @@ public class TouhouHitboxes implements CommandExecutor, Listener {
             CraftPlayer oldPlayer = (CraftPlayer) event.getPlayer();
             oldPlayer.saveData();
 
-            TouhouPlayer player = new TouhouPlayer(
-                    ((CraftWorld)oldPlayer.getWorld()).getHandle().getServer(),
-                    ((CraftWorld)oldPlayer.getWorld()).getHandle(),
-                    oldPlayer.getProfile(),
-                    oldPlayer.getHandle().clientInformation());
+            // TouhouPlayer player = new TouhouPlayer(
+            //         ((CraftWorld)oldPlayer.getWorld()).getHandle().getServer(),
+            //         ((CraftWorld)oldPlayer.getWorld()).getHandle(),
+            //         oldPlayer.getProfile(),
+            //         oldPlayer.getHandle().clientInformation());
+            TouhouPlayer player = new TouhouPlayer(oldPlayer);
 
-            player.connection=oldPlayer.getHandle().connection;
-            player.connection.player=player;
-            oldPlayer.getHandle().discard();
-            PlayerList playerList = ((CraftWorld)oldPlayer.getWorld()).getHandle().getServer().getPlayerList();
-            Bukkit.getLogger().info("[GensouJank] Removing " + event.getPlayer().getName() + " from player list...");
-            ((CraftWorld)oldPlayer.getWorld()).getHandle().getServer().getPlayerList().remove((oldPlayer).getHandle());
-            Bukkit.getLogger().info("[GensouJank] Adding " + event.getPlayer().getName() + " to player list...");
-            ((CraftWorld)oldPlayer.getWorld()).getHandle().getServer().getPlayerList().placeNewPlayer((oldPlayer).getHandle().connection.connection,player, new CommonListenerCookie((oldPlayer).getProfile(), 0, (oldPlayer).getHandle().clientInformation()));
-            Bukkit.getLogger().info("[GensouJank] Respawning " + event.getPlayer().getName() + "...");
-            playerList.respawn(player, ((CraftWorld)oldPlayer.getWorld()).getHandle().getLevel(), true, oldPlayer.getLocation(),false,PlayerRespawnEvent.RespawnReason.PLUGIN);
-
-            Bukkit.getScheduler().runTaskTimer(gensouJank, () -> {
-                var box = player.getBoundingBox();
-                var world = event.getPlayer().getWorld();
-                world.spawnParticle(Particle.REDSTONE, new Location(world, box.minX, box.minY, box.minZ), 1, 0, 0, 0, 0, new Particle.DustOptions(Color.RED, 1));
-                world.spawnParticle(Particle.REDSTONE, new Location(world, box.maxX, box.maxY, box.maxX), 1, 0, 0, 0, 0, new Particle.DustOptions(Color.BLUE, 1));
-            }, 0L, 10L);
+            // player.connection=oldPlayer.getHandle().connection;
+            // player.connection.player=player;
+            // oldPlayer.getHandle().discard();
+            // PlayerList playerList = ((CraftWorld)oldPlayer.getWorld()).getHandle().getServer().getPlayerList();
+            // Bukkit.getLogger().info("[GensouJank] Removing " + event.getPlayer().getName() + " from player list...");
+            // ((CraftWorld)oldPlayer.getWorld()).getHandle().getServer().getPlayerList().remove((oldPlayer).getHandle());
+            // Bukkit.getLogger().info("[GensouJank] Adding " + event.getPlayer().getName() + " to player list...");
+            // ((CraftWorld)oldPlayer.getWorld()).getHandle().getServer().getPlayerList().placeNewPlayer((oldPlayer).getHandle().connection.connection,player, new CommonListenerCookie((oldPlayer).getProfile(), 0, (oldPlayer).getHandle().clientInformation()));
+            // Bukkit.getLogger().info("[GensouJank] Respawning " + event.getPlayer().getName() + "...");
+            // playerList.respawn(player, ((CraftWorld)oldPlayer.getWorld()).getHandle().getLevel(), true, oldPlayer.getLocation(),false,PlayerRespawnEvent.RespawnReason.PLUGIN);
+            //
+            // Bukkit.getScheduler().runTaskTimer(gensouJank, () -> {
+            //     var box = player.getBoundingBox();
+            //     var world = event.getPlayer().getWorld();
+            //     world.spawnParticle(Particle.REDSTONE, new Location(world, box.minX, box.minY, box.minZ), 1, 0, 0, 0, 0, new Particle.DustOptions(Color.RED, 1));
+            //     world.spawnParticle(Particle.REDSTONE, new Location(world, box.maxX, box.maxY, box.maxX), 1, 0, 0, 0, 0, new Particle.DustOptions(Color.BLUE, 1));
+            // }, 0L, 10L);
         }, 0L);
     }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if(args.length < 2 || !(args[1].equalsIgnoreCase("true") || args[1].equalsIgnoreCase("false"))) {
-            sender.sendMessage(ChatColor.RED + "Usage: /touhouhitbox <player> <true|false>");
+            sender.sendMessage(
+                Component
+                    .text()
+                    .color(NamedTextColor.RED)
+                    .content("Usage: /touhouhitbox <player> <true|false>")
+                    .build()
+            );
+
             return true;
         }
 
         var player = (CraftPlayer) Bukkit.getPlayerExact(args[0]);
 
         if (player == null) {
-            sender.sendMessage(ChatColor.RED + "Player " + ChatColor.WHITE + args[0] + ChatColor.RED + " not found");
+            sender.sendMessage(
+                Component
+                    .text()
+                    .append(Component.text().color(NamedTextColor.RED).content("Player "))
+                    .append(Component.text().color(NamedTextColor.WHITE).content(args[0]))
+                    .append(Component.text().color(NamedTextColor.RED).content(" not found"))
+                    .build()
+            );
             return true;
         }
 
         if(player.getHandle() instanceof TouhouPlayer touhouPlayer) {
             touhouPlayer.bossMode=args[1].equalsIgnoreCase("true");
-            sender.sendMessage("Boss mode for " + ChatColor.GREEN + player.getName() + ChatColor.RESET + " set to " + args[1]);
+            sender.sendMessage(
+                Component
+                    .text()
+                    .append(Component.text().color(NamedTextColor.GREEN).content("Boss mode for "))
+                    .append(Component.text().color(NamedTextColor.WHITE).content(player.getName()))
+                    .append(Component.text().color(NamedTextColor.GREEN).content(" set to "))
+                    .append(Component.text().color(NamedTextColor.WHITE).content(args[1]))
+                    .build()
+            );
         } else {
             throw new IllegalStateException("Player is " + player.getHandle().getClass()+ " instead of " + TouhouPlayer.class);
         }
--- src/main/java/net/gensokyoreimagined/gensoujank/TouhouPlayer.java
+++ src/main/java/net/gensokyoreimagined/gensoujank/TouhouPlayer.java
@@ -7,20 +7,127 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.phys.AABB;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_20_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_20_R2.entity.CraftPlayer;
 import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.*;
 
 
 public class TouhouPlayer extends ServerPlayer {
-    public boolean bossMode=false;
 
-    public TouhouPlayer(MinecraftServer server, ServerLevel world, GameProfile profile, ClientInformation clientOptions) {
-        super(server, world, profile, clientOptions);
+    private static final Set<String> commonDescriptorNames = new HashSet<>();
+    private static final HashMap<String, Field> serverPlayerDescriptorMap = new HashMap<>();
+    private static final HashMap<String, Field> touhouPlayerDescriptorMap = new HashMap<>();
+
+    static {
+        // Now invoking reflection magic, please watch warmly
+
+        // TLDR: get all the property descriptors for ServerPlayer and TouhouPlayer, then get the common ones
+        var serverPlayerFields = getFields(ServerPlayer.class);
+        var touhouPlayerFields = getFields(TouhouPlayer.class);
+
+        serverPlayerFields.forEach(field -> {
+            if ((field.getModifiers() & (Modifier.STATIC | Modifier.FINAL)) == (Modifier.STATIC | Modifier.FINAL)) return; // skip static final fields (they're constants)
+
+            serverPlayerDescriptorMap.put(field.getName(), field);
+            field.setAccessible(true);
+        });
+
+        touhouPlayerFields.forEach(field -> {
+            if ((field.getModifiers() & (Modifier.STATIC | Modifier.FINAL)) == (Modifier.STATIC | Modifier.FINAL)) return; // skip static final fields (they're constants)
+
+            touhouPlayerDescriptorMap.put(field.getName(), field);
+            field.setAccessible(true);
+        });
+
+        commonDescriptorNames.addAll(serverPlayerDescriptorMap.keySet());
+        commonDescriptorNames.retainAll(touhouPlayerDescriptorMap.keySet());
+
+        // var nameList = commonDescriptorNames.stream().reduce((a, b) -> a + ", " + b).orElse("");
+        // Bukkit.getLogger().info("[GensouJank] Common fields: " + nameList);
     }
 
+    private static Iterable<Field> getFields(Class<?> startClass) {
 
+        var fields = new ArrayList<>(List.of(startClass.getDeclaredFields()));
+        Class<?> parentClass = startClass.getSuperclass();
+
+        if (parentClass != null && !parentClass.equals(Object.class)) {
+            // lol, recursion
+            var parentFields = getFields(parentClass);
+            parentFields.forEach(fields::add);
+        }
+
+        return fields;
+    }
+
+    public boolean bossMode = false;
+
+    // public TouhouPlayer(MinecraftServer server, ServerLevel world, GameProfile profile, ClientInformation clientOptions) {
+    //     super(server, world, profile, clientOptions);
+    // }
+
+    public TouhouPlayer(CraftPlayer player) {
+        super(
+            ((CraftWorld) player.getWorld()).getHandle().getServer(),
+            ((CraftWorld) player.getWorld()).getHandle(),
+            player.getProfile(),
+            player.getHandle().clientInformation()
+        );
+
+        // Using the power of reflection and bytecode manipulation, we can do this
+
+        // Copy all the common properties from the server player to the Touhou player
+        for (var name : commonDescriptorNames) {
+            var serverPlayerField = serverPlayerDescriptorMap.get(name);
+            var touhouPlayerField = touhouPlayerDescriptorMap.get(name);
+
+            if (serverPlayerField != null && touhouPlayerField != null) { // should always be true
+                try {
+                    switch (serverPlayerField.getType().getName()) {
+                        case "boolean":
+                            touhouPlayerField.setBoolean(this, serverPlayerField.getBoolean(player.getHandle()));
+                            break;
+                        case "byte":
+                            touhouPlayerField.setByte(this, serverPlayerField.getByte(player.getHandle()));
+                            break;
+                        case "char":
+                            touhouPlayerField.setChar(this, serverPlayerField.getChar(player.getHandle()));
+                            break;
+                        case "double":
+                            touhouPlayerField.setDouble(this, serverPlayerField.getDouble(player.getHandle()));
+                            break;
+                        case "float":
+                            touhouPlayerField.setFloat(this, serverPlayerField.getFloat(player.getHandle()));
+                            break;
+                        case "int":
+                            touhouPlayerField.setInt(this, serverPlayerField.getInt(player.getHandle()));
+                            break;
+                        case "long":
+                            touhouPlayerField.setLong(this, serverPlayerField.getLong(player.getHandle()));
+                            break;
+                        case "short":
+                            touhouPlayerField.setShort(this, serverPlayerField.getShort(player.getHandle()));
+                            break;
+                        default:
+                            touhouPlayerField.set(this, serverPlayerField.get(player.getHandle()));
+                            break;
+                    }
+
+                    Bukkit.getLogger().info("[GensouJank] Copied property " + name + " from ServerPlayer to TouhouPlayer");
+                } catch (Exception e) {
+                    Bukkit.getLogger().warning("[GensouJank] Failed to copy property " + name + " from ServerPlayer to TouhouPlayer\n" + e);
+                }
+            }
+        }
+    }
 
     @Override
     protected @NotNull AABB makeBoundingBox() {
-        return bossMode ? getDimensions(Pose.SWIMMING).makeBoundingBox(this.position().add(0,1.5,0)) : super.makeBoundingBox();
+        return bossMode ? getDimensions(Pose.SWIMMING).makeBoundingBox(this.position().add(0, 1.5, 0)) : super.makeBoundingBox();
     }
 }
