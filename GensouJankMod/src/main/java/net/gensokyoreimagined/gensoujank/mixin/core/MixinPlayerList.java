45b392a0b9c30b80dab3d0208d58c6a7f6382619
DoggySazHi <reimu@williamle.com>
2024-02-10 23:12:08 -0800

    Mod actually works

--- GensouJankMod/src/main/java/net/gensokyoreimagined/gensoujank/mixin/core/MixinPlayerList.java
+++ GensouJankMod/src/main/java/net/gensokyoreimagined/gensoujank/mixin/core/MixinPlayerList.java
@@ -1,19 +1,21 @@
 package net.gensokyoreimagined.gensoujank.mixin.core;
 
 import com.mojang.authlib.GameProfile;
+import net.gensokyoreimagined.gensoujank.TouhouPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ClientInformation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.server.network.ServerLoginPacketListenerImpl;
 import net.minecraft.server.players.PlayerList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(value = PlayerList.class)
+@Mixin(PlayerList.class)
 public abstract class MixinPlayerList {
-
-    @Inject(method = "canPlayerLogin", at = @At("HEAD"))
-    private void onPlayerLogin(ServerLoginPacketListenerImpl loginlistener, GameProfile gameprofile, CallbackInfoReturnable<ServerPlayer> cir) {
-        System.out.println("Player " + gameprofile.getName() + " is trying to log in!");
+    @Redirect(method = "canPlayerLogin", at = @At(value = "NEW", target = "(Lnet/minecraft/server/MinecraftServer;Lnet/minecraft/server/level/ServerLevel;Lcom/mojang/authlib/GameProfile;Lnet/minecraft/server/level/ClientInformation;)Lnet/minecraft/server/level/ServerPlayer;"))
+    private ServerPlayer onCanPlayerLogin(MinecraftServer server, ServerLevel world, GameProfile profile, ClientInformation clientOptions) {
+        System.out.println("Substituted TouhouPlayer successfully!");
+        return new TouhouPlayer(server, world, profile, clientOptions);
     }
 }
